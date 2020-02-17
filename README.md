159dd437c4da90d820fba3ca82c60ecdb82c3d31
Luis Mejia <luyisimiger@gmail.com>
2020-02-17 03:36:49 -0500

    arreglos finales

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-# monitor-ritmo-cardiaco
-Sistema Tiempo Real | Proyecto Corte 3
+# Sistema de Tiempo Real | monitor-ritmo-cardiaco - Universidad de Cartagena
 
-# camando para habilitar emulacion de dos puertos seriales
+Desarrollado por **Luis Miguel Mejia** y **Saad Bittar**
+
+## Requerimiento
+
+Desarrollar un sistema IoT para el seguimiento y análisis del ritmo cardíaco y la variación del ritmo cardíaco haciendo uso del microframework Flask. El sistema debe permitir realizar las siguientes operaciones:
+
+- Posibilitar el inicio y la detención de la captura del ritmo cardiaco (HR) y la variabilidad del ritmo cardíaco(RR).  Los datos serán recibidos al mismo tiempo. 
+- Permitir la consulta del histórico de las sesiones. Cada sesión tiene un código estampa que la distingue y está asociada a un conjunto de capturas de HR y RR.
+- Obtener para cada sesión: el promedio del HR y de RR, el valor máximo y mínimo de HR y RR,  la desviación estándar de los valores HR y RR(SDRR), el porcentaje pRR50 de la variabilidad del ritmo cardíaco. 
+- Determinar el riesgo asociado a las métricas de promedio de la variabilidad del ritmo cardíaco, SDRR y pRR50 (ver diapositiva siguiente)
+- Presentar en tiempo real las gráficas del ritmo cardíaco (HR) y de la variabilidad del ritmo cardíaco (RR).
+- Obtener los centroides de los datos del ritmo cardíaco y de la variabilidad del ritmo cardíaco. 
+
+## Comandos
+
+### camando para habilitar emulacion de dos puertos seriales
+```
 sudo socat -d -d PTY,link=/dev/ttyS23 PTY,link=/dev/ttyS24
+```
 
-# comando para ejecutar el cinturon bluetooth
+### comando para ejecutar el cinturon bluetooth
+```
 sudo python3 backend/cinturon.py
+```
 
-# comando para iniciar el worker
+### comando para iniciar el worker
+```
 sudo rq worker monitor-cardiaco -u redis://localhost
+```
 
-# comando para ejecutar el servidor de flask (backend)
+### comando para ejecutar el servidor de flask (backend)
+```
 gunicorn3 backend.monitor.app:app --reload
+```
 
-# comando para ejecutar la aplicacion de vue.js (frontend)
+### comando para ejecutar la aplicacion de vue.js (frontend)
+```
 cd frontend && npm run serve
+```
--- frontend/src/views/Layout.vue
+++ frontend/src/views/Layout.vue
@@ -6,8 +6,18 @@
       app
     >
       <v-list dense>
+        
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Luis Mejia & Saad Bittar</v-list-item-title>
+            <v-list-item-subtitle>Desarrolladores</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+        
         <v-list-item
-          v-for="item in xitems"
+          v-for="item in items"
           :key="item.text"
           :to="{ name: item.to }"
           link
@@ -19,49 +29,9 @@
             <v-list-item-title>{{ item.text }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        
       </v-list>
 
-      <v-list dense>
-        <template v-for="item in items">
-          <v-row v-if="item.heading" :key="item.heading" align="center">
-            <v-col cols="6">
-              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-col>
-          </v-row>
-          <v-list-group
-            v-else-if="item.children"
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon
-          >
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item-content>
-            </template>
-            <v-list-item v-for="(child, i) in item.children" :key="i" link>
-              <v-list-item-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>{{ child.text }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-          <v-list-item v-else :key="item.text" link>
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar
@@ -72,16 +42,8 @@
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-        <span class="hidden-sm-and-down">Google Contacts</span>
+        <span class="hidden-sm-and-down">Monitor ritmo cardiaco</span>
       </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-        label="Search"
-        class="hidden-sm-and-down"
-      />
       <v-spacer />
       <v-btn icon>
         <v-icon>mdi-apps</v-icon>
@@ -165,37 +127,7 @@ export default {
   data: () => ({
     dialog: false,
     drawer: null,
-    xitems: [{ icon: "mdi-contacts", text: "Sessions", to: "sessions-list" }],
-    items: [
-      { icon: "mdi-contacts", text: "Sessions", to: "sessions-list" },
-      { icon: "mdi-history", text: "Frequently contacted", to: "" },
-      { icon: "mdi-content-copy", text: "Duplicates", to: "" },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "Labels",
-        model: true,
-        children: [{ icon: "mdi-plus", text: "Create label" }]
-      },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "More",
-        model: false,
-        children: [
-          { text: "Import" },
-          { text: "Export" },
-          { text: "Print" },
-          { text: "Undo changes" },
-          { text: "Other contacts" }
-        ]
-      },
-      { icon: "mdi-settings", text: "Settings", to: "" },
-      { icon: "mdi-message", text: "Send feedback", to: "" },
-      { icon: "mdi-help-circle", text: "Help", to: "" },
-      { icon: "mdi-cellphone-link", text: "App downloads", to: "" },
-      { icon: "mdi-keyboard", text: "Go to the old version", to: "" }
-    ]
+    items: [{ icon: "mdi-contacts", text: "Sessions", to: "sessions-list" }]
   })
 };
 </script>
